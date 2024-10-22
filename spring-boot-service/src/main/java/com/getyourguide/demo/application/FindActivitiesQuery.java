58b4c26d89fcd42a87eed45925eb9f124e188ff7
Elises <yeghishe.kartashyan@gmail.com>
2024-10-22 16:31:23 +0200

    refactor: update FindActivitiesQuery class to make the searchTerm, page, and size fields private and provide getter methods for them.

--- spring-boot-service/src/main/java/com/getyourguide/demo/application/FindActivitiesQuery.java
+++ spring-boot-service/src/main/java/com/getyourguide/demo/application/FindActivitiesQuery.java
@@ -1,13 +1,25 @@
 package com.getyourguide.demo.application;
 
 public class FindActivitiesQuery {
-  public final String searchTerm;
-  public final int page;
-  public final int size;
+  private final String searchTerm;
+  private final int page;
+  private final int size;
 
   public FindActivitiesQuery(String searchTerm, int page, int size) {
-      this.searchTerm = searchTerm;
-      this.page = page;
-      this.size = size;
+    this.searchTerm = searchTerm;
+    this.page = page;
+    this.size = size;
+  }
+
+  public String getSearchTerm() {
+    return searchTerm;
+  }
+
+  public int getPage() {
+    return page;
+  }
+
+  public int getSize() {
+    return size;
   }
 }

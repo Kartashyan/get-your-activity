347aee06596263c33190f67f21cadfc86d347d58
Elises <yeghishe.kartashyan@gmail.com>
2024-10-22 16:32:38 +0200

    refactor: make FindActivitiesResult fields private and provide getter methods

--- spring-boot-service/src/main/java/com/getyourguide/demo/application/FindActivitiesResult.java
+++ spring-boot-service/src/main/java/com/getyourguide/demo/application/FindActivitiesResult.java
@@ -4,11 +4,11 @@ import com.getyourguide.demo.domain.Activity;
 import java.util.List;
 
 public class FindActivitiesResult {
-  final List<Activity> activities;
-  final long total;
-  final int totalPages;
-  final String next;
-  final String prev;
+  private final List<Activity> activities;
+  private final long total;
+  private final int totalPages;
+  private final String next;
+  private final String prev;
 
   public FindActivitiesResult(List<Activity> activities, long total, int totalPages, String next, String prev) {
     this.activities = activities;
@@ -16,5 +16,25 @@ public class FindActivitiesResult {
     this.totalPages = totalPages;
     this.next = next;
     this.prev = prev;
+  }
+
+  public List<Activity> getActivities() {
+    return activities;
+  }
+
+  public long getTotal() {
+    return total;
+  }
+
+  public int getTotalPages() {
+    return totalPages;
+  }
+
+  public String getNext() {
+    return next;
+  }
+
+  public String getPrev() {
+    return prev;
   }
 }
